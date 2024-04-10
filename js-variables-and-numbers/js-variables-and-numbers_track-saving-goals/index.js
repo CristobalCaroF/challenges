30ed188c918c81296d7d62552525a1536ea81dac
Cristobal Caro <cacaro@uc.cl>
2024-04-10 12:10:58 +0200

    js track saving goals challenge

---
+++ js-variables-and-numbers/js-variables-and-numbers_track-saving-goals/index.js
@@ -0,0 +1,9 @@
+const startingBalance = 1000;
+const monthlyContribution = 100;
+const months = 12;
+let totalSavings = startingBalance + monthlyContribution * months;
+console.log(
+  "After 12 months of saving, you will have",
+  totalSavings,
+  "in your account."
+);
