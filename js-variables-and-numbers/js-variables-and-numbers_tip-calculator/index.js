b36e506cbc0a0e08532b9e8211fb1b53fed76e68
Cristobal Caro <cacaro@uc.cl>
2024-04-11 15:43:43 +0200

    added changes from review: currency to return sentence

--- js-variables-and-numbers/js-variables-and-numbers_tip-calculator/index.js
+++ js-variables-and-numbers/js-variables-and-numbers_tip-calculator/index.js
@@ -5,4 +5,4 @@ const tipPercentage = 0.1;
 let tipAmount = mealCost * tipPercentage;
 // console.log("meal tip amount: ", tipAmount);
 let totalCost = mealCost + tipAmount;
-console.log("The total cost of your bill is: ", totalCost);
+console.log("The total cost of your bill is:", totalCost, "euros.");
