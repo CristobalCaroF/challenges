53fecace702b13bd1e53c11359bac748b23fc829
Cristobal Caro <cacaro@uc.cl>
2024-04-16 15:36:09 +0200

    js forms handle form submit challenge

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  event.target.elements.firstName.focus();
+  const ageBadnessSum =
+    +event.target.elements.badness.value + +event.target.elements.age.value;
+
+  console.log(
+    `The age-badness-sum of ${event.target.elements.firstName.value} is ${ageBadnessSum}`
+  );
+
+  event.target.reset();
+});
