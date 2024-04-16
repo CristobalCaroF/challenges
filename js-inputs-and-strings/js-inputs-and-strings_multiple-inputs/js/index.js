4d90f55446c1793581dd52a240a8d85f86b68293
Cristobal Caro <cacaro@uc.cl>
2024-04-16 12:03:29 +0200

    js inputs multiple inputs 3rd sub challenge

--- js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,10 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click", () => {
+  const firstInputNewValue = secondInput.value;
+  const secondInputNewValue = firstInput.value;
+  firstInput.value = firstInputNewValue;
+  secondInput.value = secondInputNewValue;
+});
