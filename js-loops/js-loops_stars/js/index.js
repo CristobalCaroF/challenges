2fb39f9b8503066947cad044983667d60e97795d
Cristobal Caro <cacaro@uc.cl>
2024-04-17 16:29:31 +0200

    s loops stars challenge part 2

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,18 +2,26 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  countFilled = filledStars;
   for (i = 1; i <= 5; i++) {
-    const star = document.createElement("img");
-    star.setAttribute("src", "assets/star-empty.svg");
-    starContainer.append(star);
+    if (countFilled > 0) {
+      const star = document.createElement("img");
+      star.setAttribute("src", "assets/star-filled.svg");
+      starContainer.append(star);
+      countFilled--;
+    } else {
+      const star = document.createElement("img");
+      star.setAttribute("src", "assets/star-empty.svg");
+      starContainer.append(star);
+    }
   }
 
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(3);
