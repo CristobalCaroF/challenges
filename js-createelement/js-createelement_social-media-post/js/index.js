95b1de3c5826956e3f3a2ee41566b6598c33d5af
Cristobal Caro <cacaro@uc.cl>
2024-04-17 12:48:49 +0200

    js create element social media post challenge

--- js-createelement/js-createelement_social-media-post/js/index.js
+++ js-createelement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,27 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const sectionElement = document.createElement("section");
+sectionElement.classList.add("post");
+
+const pElement = document.createElement("p");
+pElement.classList.add("post__content");
+pElement.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+const footerElement = document.createElement("footer");
+footerElement.classList.add("post__footer");
+
+const spanElement = document.createElement("span");
+spanElement.classList.add("post__username");
+spanElement.textContent = "@newusername";
+
+const buttonElement = document.createElement("button");
+buttonElement.classList.add("post__button");
+buttonElement.textContent = "♥ Like";
+buttonElement.addEventListener("click", handleLikeButtonClick);
+
+document.body.append(sectionElement);
+sectionElement.append(pElement, footerElement);
+footerElement.append(spanElement, buttonElement);
