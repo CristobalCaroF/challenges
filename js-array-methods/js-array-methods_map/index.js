341c266f0452125b0d1500bab94657d6d23b30f9
Cristobal Caro <cacaro@uc.cl>
2024-04-22 15:54:40 +0200

    js array methods map challenge

--- js-array-methods/js-array-methods_map/index.js
+++ js-array-methods/js-array-methods_map/index.js
@@ -24,10 +24,19 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+}); // ['as often as you like.', ...]
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return card.question + " - " + card.answer;
+}); // ["How often can I use <header>? - As often as you like.", ...]
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  let Object = {};
+  Object.question = card.question;
+  Object.answer = card.answer;
+  return Object;
+}); // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
