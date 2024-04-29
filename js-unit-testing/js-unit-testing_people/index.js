c0f0f05ab430d0d8c995d13a30cac201a460478b
Cristobal Caro <cacaro@uc.cl>
2024-04-29 12:56:03 +0200

    unit testing people challenge

--- js-unit-testing/js-unit-testing_people/index.js
+++ js-unit-testing/js-unit-testing_people/index.js
@@ -2,18 +2,72 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  let peopleFullNames = [];
+  for (const person of people) {
+    let fullName = person.firstName + " " + person.lastName;
+    peopleFullNames.push(fullName);
+  }
+  return peopleFullNames;
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  let peopleNameandAge = [];
+  for (const person of people) {
+    let nameAndAge = person.lastName + " (" + person.age + ")";
+    peopleNameandAge.push(nameAndAge);
+  }
+  return peopleNameandAge;
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  let peopleByAge = [];
+  for (let person of people) {
+    if (person.age === age) {
+      let fullName = person.firstName + " " + person.lastName;
+      peopleByAge.push(fullName);
+    }
+  }
+  return peopleByAge;
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  let peopleNamesOlderThan = [];
+  for (let person of people) {
+    if (person.age > age) {
+      let fullName = person.firstName + " " + person.lastName;
+      peopleNamesOlderThan.push(fullName);
+    }
+  }
+  return peopleNamesOlderThan;
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) {
+  let peopleByLastName = [];
+  for (let person of people) {
+    if (person.lastName === lastName) {
+      let fullName = person.firstName + " " + person.lastName;
+      peopleByLastName.push(fullName);
+    }
+  }
+  return peopleByLastName;
+}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  return people.find((person) => person.id === id);
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > age);
+}
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  let peopleSortedByAge = [];
+  const sortedByAge = people.slice().sort((a, b) => a.age - b.age);
+  console.log(sortedByAge);
+  for (const person of sortedByAge) {
+    let fullName = person.firstName + " " + person.lastName;
+    peopleSortedByAge.push(fullName);
+  }
+  return peopleSortedByAge;
+}
