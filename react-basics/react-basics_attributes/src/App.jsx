e6b37d0835ba9f7dbe0e65730505c5c121646335
Cristobal Caro <cacaro@uc.cl>
2024-05-03 15:53:37 +0200

    react basics attributes challenge

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Best title ever</h2>
+      <label htmlFor="input">Input label</label>
+      <input id="input"></input>
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/French_fries"
+      >
+        Link to your heart
+      </a>
+    </article>
+  );
 }
