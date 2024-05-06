f46194f3ceff5ae67d68ec95e272d362226a6d9d
Cristobal Caro <cacaro@uc.cl>
2024-05-06 16:53:09 +0200

    react nesting fragments challenge

--- react-nesting/react-nesting_fragments/src/App.jsx
+++ react-nesting/react-nesting_fragments/src/App.jsx
@@ -1,12 +1,21 @@
+import { Fragment } from "react";
 import "./App.css";
 import Box from "./components/Box";
 
 export default function App() {
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <Boxes />
     </main>
+  );
+}
+
+function Boxes() {
+  return (
+    <>
+      <Box color="#007bff"></Box>
+      <Box color="#fc3"></Box>
+      <Box color="#ff3333"></Box>
+    </>
   );
 }
