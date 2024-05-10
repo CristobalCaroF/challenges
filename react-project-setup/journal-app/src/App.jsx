72ed271227564e9a51996aaccf57e784dbab5eec
Cristobal Caro <cacaro@uc.cl>
2024-05-10 10:13:31 +0200

    react state and arrays challenges added

---
+++ react-project-setup/journal-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <body>
+        <header>
+          <h1>Journal</h1>
+        </header>
+        <main>
+          <h2>NEW ENTRY</h2>
+          <label htmlFor="">Motto</label>
+          <form action="">
+            <textarea name="" id=""></textarea>
+          </form>
+          <label htmlFor="">Notes</label>
+          <form action="">
+            <textarea name="" id=""></textarea>
+          </form>
+          <button>Create</button>
+        </main>
+        <footer>Journal App - 2028</footer>
+      </body>
+    </>
+  );
+}
+
+export default App;
